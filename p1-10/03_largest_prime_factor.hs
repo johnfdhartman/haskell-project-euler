42b5eb44dda7ecf46950f6f7acb2f566b148dc52
John Hartman <john.f.d.hartman@gmail.com>
2018-01-17 14:01:17 +1000

    this is unexpectedly difficult

--- p1-10/03_largest_prime_factor.hs
+++ p1-10/03_largest_prime_factor.hs
@@ -1,5 +1,24 @@
-primeFactors :: Int -> Int
-primeFactors n
-  | factors == [n] = n
-  | otherwise = maximum [ primeFactors f | f <- factors, f /= n]
-  where factors = [m | m <- [2..n], n `mod` m == 0]
+-- Too slow to be feasible
+
+-- primeFactors :: Int -> Int
+-- primeFactors n
+--   | factors == [n] = n
+--   | otherwise = maximum [ primeFactors f | f <- factors, f /= n]
+--   where factors = [m | m <- [2..n], n `mod` m == 0]
+--
+
+-- Faster without iteration?
+
+smallestFactorHigherThan :: Int -> Int -> Int
+smallestFactorHigherThan n m
+  | n `mod` m == 0 = m
+  | otherwise = smallestFactorHigherThan n (m + 1)
+
+smallestFactor :: Int -> Int
+smallestFactor n = smallestFactorHigherThan n 2
+
+largestPrimeFactor :: Int -> Int
+largestPrimeFactor n = let smallest = smallestFactor n in
+  if smallest == n
+    then n
+  else largestPrimeFactor(n `quot` smallest)
